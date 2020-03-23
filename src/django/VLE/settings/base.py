85fec5a2ca957932bb1397823541d0514291ded9
M.J. van Keulen <mjvkeulen1@gmail.com>
2020-03-23 12:51:17 +0100

    Revert back to default static directory

--- src/django/VLE/settings/base.py
+++ src/django/VLE/settings/base.py
@@ -23,11 +23,6 @@ sentry_sdk.init(
 )
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
 BASELINK = os.environ['BASELINK']
 API_URL = os.environ['API_URL']
 
